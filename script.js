c0cd277ddba2c3d180ff42549174e6cb6ea2a161
Elie Daviron <elie.daviron@gmail.com>
2017-06-22 18:32:16 +0200

    Récupérer le nombre de coopérateurs à partir de l'api de Github

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const url = "https://api.github.com/repos/edacook/exercice_prog/contents/board.html?ref=master"
+
+loadCoopNumber = () =>
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>
+        printCooperatorNumber(data.size)
+    )
+
 printCooperatorNumber = number =>
     document.querySelector("#coop-number").innerText = number
 
 document.addEventListener("DOMContentLoaded", () => 
-    printCooperatorNumber(23)
+    loadCoopNumber(url)
 )
-
